a7fa5b4396f9ca1425d596eb985123ed02af7847
Shira Gold <shira14@gmail.com>
2024-04-12 13:45:21 +0300

    Home layout done

--- Jon50/src/views/HomeView.vue
+++ Jon50/src/views/HomeView.vue
@@ -6,12 +6,12 @@
     <h3 class="home-view-title">We're thrilled to celebrate with you...</h3>
     <h3 class="home-view-title">Wishing all the best, to another exciting decade!</h3>
     <RouterLink to="/content" class="home-view-play">
-      <button>
+      <button class="button">
         <img class="icon large" src="../assets/icons/music.svg" alt="Play music only" />
       </button>
     </RouterLink>
     <RouterLink to="/content/acapella" class="home-view-play">
-      <button>
+      <button class="button">
         <img class="icon large" src="../assets/icons/mic.svg" alt="Play with singer" />
       </button>
     </RouterLink>
@@ -22,13 +22,14 @@
 .home-view {
   height: calc(100vh - 2rem);
   display: grid;
-  grid-template-rows: 8rem 2rem 2rem 20rem;
-  grid-template-columns: repeat(2, 20rem);
+  grid-template-rows: 8rem 2rem 2rem 16rem;
+  grid-template-columns: repeat(2, 16rem);
   grid-gap: 0.5rem 2rem;
   grid-template-areas: 'header header' 'title1 title1' 'title2 title2' 'play1 play2';
   grid-template-areas: 1fr 1fr;
   place-content: center;
-  align-items: center;
+  align-items: start;
+  justify-items: center;
 
   .home-view-header {
     text-align: center;
@@ -42,6 +43,10 @@
 
   .home-view-title:last-of-type {
     grid-area: title2;
+  }
+
+  .home-view-play {
+    align-self: center;
   }
 
   .home-view-play:first-of-type {
