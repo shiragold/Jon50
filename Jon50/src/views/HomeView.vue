236ba5cb238aedc99dfc67d8455293b17200bc76
Shira Gold <shira14@gmail.com>
2024-04-12 07:12:31 +0300

    grid areas

--- Jon50/src/views/HomeView.vue
+++ Jon50/src/views/HomeView.vue
@@ -2,36 +2,54 @@
 
 <template>
   <div class="home-view">
-    <h1 class="home-view-h1">Sing-a-long</h1>
-    <h3>We're thrilled to celebrate with you...</h3>
-    <h3>Wishing all the best, to another exciting decade!</h3>
-    <div>
-      <RouterLink to="/content">
-        <button>
-          <img class="icon large" src="../assets/icons/music.svg" alt="Play music only" />
-        </button>
-      </RouterLink>
-      <RouterLink to="/content/acapella">
-        <button>
-          <img class="icon large" src="../assets/icons/mic.svg" alt="Play with singer" />
-        </button>
-      </RouterLink>
-    </div>
+    <h1 class="home-view-header">Sing-a-long</h1>
+    <h3 class="home-view-title">We're thrilled to celebrate with you...</h3>
+    <h3 class="home-view-title">Wishing all the best, to another exciting decade!</h3>
+    <RouterLink to="/content" class="home-view-play">
+      <button>
+        <img class="icon large" src="../assets/icons/music.svg" alt="Play music only" />
+      </button>
+    </RouterLink>
+    <RouterLink to="/content/acapella" class="home-view-play">
+      <button>
+        <img class="icon large" src="../assets/icons/mic.svg" alt="Play with singer" />
+      </button>
+    </RouterLink>
   </div>
 </template>
 
 <style>
 .home-view {
-  height: 100vh;
+  height: calc(100vh - 2rem);
   display: grid;
   grid-template-rows: 8rem 2rem 2rem 20rem;
+  grid-template-columns: repeat(2, 20rem);
+  grid-gap: 0.5rem 2rem;
+  grid-template-areas: 'header header' 'title1 title1' 'title2 title2' 'play1 play2';
   grid-template-areas: 1fr 1fr;
   place-content: center;
   align-items: center;
 
-  .home-view-h1 {
+  .home-view-header {
     text-align: center;
     font-size: 4rem;
+    grid-area: header;
+  }
+
+  .home-view-title:first-of-type {
+    grid-area: title1;
+  }
+
+  .home-view-title:last-of-type {
+    grid-area: title2;
+  }
+
+  .home-view-play:first-of-type {
+    grid-area: play1;
+  }
+
+  .home-view-play:last-of-type {
+    grid-area: play2;
   }
 }
 </style>
