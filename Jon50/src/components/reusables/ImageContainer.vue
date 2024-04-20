6d098e24f7b7f078cde23a2b2baf3471d802491f
Shira Gold <shira14@gmail.com>
2024-04-20 19:50:52 +0300

    New component

---
+++ Jon50/src/components/reusables/ImageContainer.vue
@@ -0,0 +1,35 @@
+<script setup>
+defineProps({
+  image: String
+})
+</script>
+
+<template>
+  <div class="image-container">
+    <Transition>
+      <img v-if="image" class="image" :src="image" />
+    </Transition>
+  </div>
+</template>
+
+<style>
+.image-container {
+  height: 100%;
+  padding: 1rem 0;
+
+  .image {
+    height: 100%;
+    border-radius: calc(1rem * infinity);
+  }
+
+  .v-enter-active,
+  .v-leave-active {
+    transition: opacity 0.5s ease;
+  }
+
+  .v-enter-from,
+  .v-leave-to {
+    opacity: 0;
+  }
+}
+</style>
