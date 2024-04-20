edbfbc1f2e054cebd4681f0f2425f3b19cbc3eb5
Shira Gold <shira14@gmail.com>
2024-04-20 23:14:41 +0300

    Images added

---
+++ Jon50/src/assets/images/index.js
@@ -0,0 +1,74 @@
+import shabbat from '@images/shabbat.jpg'
+import tiyulim from '@images/tiyulim.jpg'
+import lishon from '@images/lishon.jpg'
+import tochnit from '@images/tochnit.jpg'
+import rishon from '@images/rishon.jpg'
+import shvilim from '@images/shvilim.jpg'
+import amiti from '@images/amiti.jpg'
+import tivee from '@images/tivee.jpg'
+import beiti from '@images/beiti.jpg'
+
+import bait1 from '@images/bait1.jpg'
+import ogen1 from '@images/ogen1.jpg'
+import hashraa1 from '@images/hashraa1.jpg'
+import lekachev1 from '@images/lekachev1.jpg'
+
+import meshatef from '@images/meshatef.jpg'
+import bachutz from '@images/bachutz.jpg'
+import mitlahavot from '@images/mitlahavot.jpg'
+import kibutz from '@images/kibutz.jpg'
+import hair from '@images/hair.jpg'
+import gilta from '@images/gilta.jpg'
+import barosh from '@images/barosh.jpg'
+import bilta from '@images/bilta.jpg'
+
+import bait2 from '@images/bait2.jpg'
+import ogen2 from '@images/ogen2.jpg'
+import hashraa2 from '@images/hashraa2.jpg'
+import lekachev2 from '@images/lekachev2.jpg'
+
+import gadol from '@images/gadol.jpg'
+import chaverim from '@images/chaverim.jpg'
+import maagalim from '@images/maagalim.jpg'
+import mifgash from '@images/mifgash.jpg'
+import aliz from '@images/aliz.jpg'
+import anglia from '@images/anglia.jpg'
+import zariz from '@images/zariz.jpg'
+
+export default [
+  shabbat,
+  tiyulim,
+  lishon,
+  tochnit,
+  rishon,
+  shvilim,
+  amiti,
+  tivee,
+  beiti,
+  bait1,
+  ogen1,
+  hashraa1,
+  hashraa1,
+  lekachev1,
+  meshatef,
+  bachutz,
+  mitlahavot,
+  kibutz,
+  hair,
+  gilta,
+  barosh,
+  bilta,
+  bait2,
+  ogen2,
+  hashraa2,
+  hashraa2,
+  lekachev2,
+  gadol,
+  gadol,
+  chaverim,
+  maagalim,
+  mifgash,
+  aliz,
+  anglia,
+  zariz
+]
