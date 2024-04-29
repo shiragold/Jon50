bf37f72e5a3414f51399b77ca5cad4ecc6d19246
Shira Gold <shira14@gmail.com>
2024-04-29 21:36:16 +0300

    Move project to main

--- Jon50/src/components/content/AcapellaPlayer.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <audio>
-    <source src="@media/acapella.mp3" type="audio/mpeg" />
-  </audio>
-</template>
--- Jon50/src/components/content/InstrumentalPlayer.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <audio>
-    <source src="@media/instrumental.mp3" type="audio/mpeg" />
-  </audio>
-</template>
