834ef56722be6e70489feb44e7ae1c9d58c1aa3c
Shira Gold <shira14@gmail.com>
2024-04-30 08:41:10 +0300

    Fix AudioPlayer.vue - import audio statically and use dynamically

--- src/components/content/AudioPlayer.vue
+++ src/components/content/AudioPlayer.vue
@@ -1,20 +1,21 @@
 <script setup>
 import { ref, onBeforeMount, onMounted } from 'vue'
+import tune from '@media/tune.mp3'
+import song from '@media/song.mp3'
+
 const props = defineProps({
   src: {
     type: String,
     required: true
   }
 })
-let audioFile = ref(null)
-onBeforeMount(() => audioFile = import('@media/' + props.src + '.mp3'))
-
 onMounted(() => {
   document.getElementsByTagName('audio')[0].play()
 })
 </script>
 <template>
   <audio>
-    <source :src="audioFile" type="audio/mpeg" />
+    <source v-if="src === 'tune'" :src="tune" type="audio/mpeg" />
+    <source v-if="src === 'song'" :src="song" type="audio/mpeg" />
   </audio>
 </template>
