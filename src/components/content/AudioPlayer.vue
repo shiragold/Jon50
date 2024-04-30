13854592a29535cc586ac7cbce8d7c0a4c865004
Shira Gold <shira14@gmail.com>
2024-04-30 08:47:42 +0300

    Fix AudioPlayer.vue - make the import outside

--- src/components/content/AudioPlayer.vue
+++ src/components/content/AudioPlayer.vue
@@ -1,7 +1,5 @@
 <script setup>
-import { ref, onBeforeMount, onMounted } from 'vue'
-import tune from '@media/tune.mp3'
-import song from '@media/song.mp3'
+import { onMounted } from 'vue'
 
 const props = defineProps({
   src: {
@@ -15,7 +13,6 @@ onMounted(() => {
 </script>
 <template>
   <audio>
-    <source v-if="src === 'tune'" :src="tune" type="audio/mpeg" />
-    <source v-if="src === 'song'" :src="song" type="audio/mpeg" />
+    <source :src="src" type="audio/mpeg" />
   </audio>
 </template>
